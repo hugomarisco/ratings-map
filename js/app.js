b0cd16b59cb269f5d5fea57d5a240c50f6ffeb60
Hugo Marisco <0x6875676f@gmail.com>
2015-12-30 17:20:27 +0000

    change xhr url

---
+++ js/app.js
@@ -0,0 +1,48 @@
+var feedbackApp = angular.module('feedbackApp', ['uiGmapgoogle-maps']);
+
+feedbackApp.config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        //    key: 'your api key',
+        v: '3.20', //defaults to latest 3.X anyhow
+        libraries: 'weather,geometry,visualization'
+    });
+});
+
+feedbackApp.controller('MainCtrl', function ($scope, $http) {
+  $scope.items = [];
+  $scope.average = 0.0;
+  $scope.ratingLevels = [[],[],[],[],[]];
+  $scope.map = { center: { latitude: 0, longitude: 0 }, zoom: 2 };
+
+  $scope.$watch('items', function(newItems, oldItems) {
+    console.log(newItems);
+    var sum = 0;
+
+    for (var i = 0; i < newItems.length; i++) {
+      var item = newItems[i];
+
+      sum += item.rating;
+
+      $scope.ratingLevels[item.rating - 1].push(item);
+    }
+
+    $scope.average = sum / newItems.length;
+  });
+
+  $scope.getItemClass = function(index) {
+    switch (index) {
+      case 0: return "red-bg"; break;
+      case 1: return "orange-bg"; break;
+      case 2: return "dark-yellow-bg"; break;
+      case 3: return "yellow-bg"; break;
+      case 4: return "green-bg"; break;
+      default: return "";
+    }
+  }
+
+  $http.get('http://usabilla.hugomarisco.in:4567/data').then(function(res) {
+    $scope.items = res.data;
+  }, function(err) {
+    console.log(err);
+  });
+});
