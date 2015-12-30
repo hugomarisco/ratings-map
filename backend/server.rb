4d4935a5216ba4210a7cf332b09ed2ceb4568a5d
Hugo Marisco <0x6875676f@gmail.com>
2015-12-30 17:09:53 +0000

    initial commit

---
+++ backend/server.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+require 'sinatra/json'
+require 'sinatra/cross_origin'
+require 'net/http'
+require 'json'
+
+configure do
+  enable :cross_origin
+end
+
+get '/data' do
+  info = JSON.parse(Net::HTTP.get('cache.usabilla.com', '/example/apidemo.json'))
+
+  new_info = []
+
+  info['items'].each do |item|
+    new_info << {
+      id: item['id'],
+      browser: item['computed_browser']['Browser'],
+      browser_version: item['computed_browser']['Version'],
+      geo_location: { latitude: item['geo']['lat'], longitude: item['geo']['lon'] },
+      rating: item['rating'],
+      labels: item['labels']
+    }
+  end
+
+  json new_info
+end
